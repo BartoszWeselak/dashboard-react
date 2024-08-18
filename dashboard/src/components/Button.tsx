2b7137ae4653327f86eb7edcd824fa1b2f0b0404
Bartosz Weselak <toszek2000@gmail.com>
2024-08-18 16:13:54 +0200

    Add button component

---
+++ dashboard/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React, { Children } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  className?: string;
+  onClick: () => void;
+}
+
+export function Button({ className, onClick, children }: ButtonProps) {
+  return (
+    <button className={twMerge("", className)} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
