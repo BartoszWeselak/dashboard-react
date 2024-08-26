b9eb417d9f26863a430e20b4228aeed62ae6490e
Bartosz Weselak <toszek2000@gmail.com>
2024-08-26 21:10:28 +0200

    Add button variants

--- dashboard/src/components/Button.tsx
+++ dashboard/src/components/Button.tsx
@@ -1,17 +1,41 @@
+import { cva, VariantProps } from "class-variance-authority";
 import React, { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+const buttonVariants = cva("w-full hover:opacity-80", {
+  variants: {
+    variant: {
+      primary: "bg-blue-500 text-white",
+      secondary: "bg-gray-500 text-white",
+      danger: "bg-red-500 text-white",
+      tab: "bg-white text-black border-2 border-gray-200 rounded-full m-6",
+    },
+    size: {
+      small: "px-2 py-1 text-sm",
+      medium: "px-4 py-2 text-base",
+      large: "px-6 py-3 text-lg",
+      xlarge: " py-4 text-base",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "medium",
+  },
+});
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   children?: React.ReactNode;
   className?: string;
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, ...props }, ref) => {
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, variant, size, ...props }, ref) => {
     return (
       <button
         ref={ref}
-        className={twMerge("w-full hover:opacity-80", className)}
+        className={twMerge(buttonVariants({ variant, size }), className)}
         {...props}
       >
         {children}
@@ -21,3 +45,5 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 );
 
 Button.displayName = "Button";
+
+export { Button, buttonVariants };
