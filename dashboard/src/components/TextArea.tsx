1805178c532600af75eb561e28aee356ec0b5f41
Bartosz Weselak <toszek2000@gmail.com>
2024-08-27 21:53:32 +0200

    Add textarea component

---
+++ dashboard/src/components/TextArea.tsx
@@ -0,0 +1,49 @@
+import { cva, VariantProps } from "class-variance-authority";
+import React, { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+const textAreaVariants = cva("text-black", {
+  variants: {
+    size: {
+      small: "w-1/6",
+      medium: "w-1/5",
+      big: "w-1/4",
+    },
+    align: {
+      right: "text-right",
+      left: "text-left",
+      center: "text-center",
+    },
+    colors: {
+      none: "bg-inherit",
+      white: "bg-white",
+      gray: "bg-gray-400 text-white",
+    },
+  },
+});
+
+interface TextAreaProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof textAreaVariants> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+let test = () => {
+  return <p className="w-1/6"></p>;
+};
+
+export const TextArea = forwardRef<HTMLDivElement, TextAreaProps>(
+  ({ children, className, size, colors, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={twMerge(textAreaVariants({ size, colors }), className)}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+TextArea.displayName = "TextArea";
