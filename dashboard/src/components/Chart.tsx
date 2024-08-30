216eada1433b92d13c0392600b73522fa1c0f0a5
Bartosz Weselak <toszek2000@gmail.com>
2024-08-30 15:11:39 +0200

    Add highcharts lib

--- dashboard/src/components/Chart.tsx
+++ dashboard/src/components/Chart.tsx
@@ -1,30 +1,6 @@
-import React, { forwardRef } from "react";
+import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-
-const chartOptions: Highcharts.Options = {
-  chart: {
-    type: "line",
-  },
-  title: {
-    text: "My Line Chart",
-  },
-  xAxis: {
-    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-  },
-  yAxis: {
-    title: {
-      text: "Value",
-    },
-  },
-  series: [
-    {
-      type: "line",
-      name: "Series 1",
-      data: [1, 2, 3, 4, 5, 6],
-    },
-  ],
-};
 
 interface ChartProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
@@ -32,13 +8,28 @@ interface ChartProps extends React.HTMLAttributes<HTMLDivElement> {
   options?: Highcharts.Options;
 }
 
-export const Chart = forwardRef<HTMLDivElement, ChartProps>(
-  ({ children, className, options, ...props }, ref) => {
-    return (
-      <div ref={ref} className={className} {...props}>
-        <HighchartsReact highcharts={Highcharts} options={chartOptions} />
-        {children}
-      </div>
-    );
-  }
-);
+const defaultChartOptions: Highcharts.Options = {
+  title: {
+    text: "Simple Line Chart",
+  },
+  series: [
+    {
+      type: "line",
+      data: [1, 2, 3, 4, 5],
+      name: "Sample Data",
+    },
+  ],
+};
+
+export const Chart = ({
+  children,
+  className,
+  options = defaultChartOptions,
+  ...props
+}: ChartProps) => {
+  return (
+    <div className={className} {...props}>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
+};
