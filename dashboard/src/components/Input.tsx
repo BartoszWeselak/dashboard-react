c35e7ad4c940a5a05af09f0aa55712a3dec29c19
Bartosz Weselak <toszek2000@gmail.com>
2024-09-04 14:38:06 +0200

    Update input component

--- dashboard/src/components/Input.tsx
+++ dashboard/src/components/Input.tsx
@@ -4,14 +4,20 @@ import { twMerge } from "tailwind-merge";
 interface InputProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
   placeHolder?: string;
+  type?: string;
+  value?: any;
+  onChange?: (e: any) => void;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, placeHolder, ...props }, ref) => {
+  ({ className, placeHolder, type, value, onChange, ...props }, ref) => {
     return (
       <input
         placeholder={placeHolder}
         ref={ref}
+        type={type}
+        value={value}
+        onChange={onChange}
         className={twMerge("w-full bg-gray-200", className)}
         {...props}
       />
